5ed8cb211626877dad5a78295cabfd39935d728c
Calleb Joel Miquissene <callebmiquissene@gmail.com>
2021-05-07 14:33:38 +0200

    Initial Commit

---
+++ app/src/main/java/com/callebdev/awesomefilters/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.callebdev.awesomefilters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
