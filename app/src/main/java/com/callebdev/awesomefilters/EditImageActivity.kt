d23a0205bf616fe43a1aed64f9369e8022d55e96
Calleb Joel Miquissene <callebmiquissene@gmail.com>
2021-05-11 01:59:12 +0200

    Pick image and display it in Edit Image Activity UI

--- app/src/main/java/com/callebdev/awesomefilters/EditImageActivity.kt
+++ app/src/main/java/com/callebdev/awesomefilters/EditImageActivity.kt
@@ -1,11 +1,36 @@
 package com.callebdev.awesomefilters
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.callebdev.awesomefilters.databinding.ActivityEditImageBinding
 
 class EditImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_image)
+        binding = ActivityEditImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        displayImagePreview()
+    }
+
+    private fun displayImagePreview() {
+        intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.imagePreview.setImageBitmap(bitmap)
+            binding.imagePreview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setListeners() {
+        binding.imageBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
