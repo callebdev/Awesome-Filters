a1a4149611de51581ec8031467b197c9dc2e35ad
Calleb Joel Miquissene <callebmiquissene@gmail.com>
2021-05-11 01:38:32 +0200

    Removed Koin dependency, edit new button click listener

---
+++ app/src/main/java/com/callebdev/awesomefilters/EditImageActivity.kt
@@ -0,0 +1,11 @@
+package com.callebdev.awesomefilters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_image)
+    }
+}
--- app/src/main/java/com/callebdev/awesomefilters/MainActivity.kt
+++ app/src/main/java/com/callebdev/awesomefilters/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.callebdev.awesomefilters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.callebdev.awesomefilters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_PICK_IMAGE = 1
+        const val KEY_IMAGE_URI = "imageUri"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.buttonEditNewImage.setOnClickListener {
+            Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            ).also { pickerIntent ->
+                pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
+            data?.data?.let { imageUri ->
+                Intent(applicationContext, EditImageActivity::class.java).also { editImageIntent ->
+                    editImageIntent.putExtra(KEY_IMAGE_URI, imageUri)
+                    startActivity(editImageIntent)
+                }
+            }
+        }
+
     }
 }
