6b18e13b2f916d6ff310133ac02e9f018d5b190b
Calleb Joel Miquissene <callebmiquissene@gmail.com>
2021-05-11 15:05:05 +0200

    Highlight selected filter's name

--- app/src/main/java/com/callebdev/awesomefilters/adapters/ImageFiltersAdapter.kt
+++ app/src/main/java/com/callebdev/awesomefilters/adapters/ImageFiltersAdapter.kt
@@ -1,8 +1,11 @@
 package com.callebdev.awesomefilters.adapters
 
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.callebdev.awesomefilters.R
 import com.callebdev.awesomefilters.data.ImageFilter
 import com.callebdev.awesomefilters.databinding.ItemContainerFilterBinding
 import com.callebdev.awesomefilters.listeners.ImageFilterListener
@@ -11,6 +14,9 @@ class ImageFiltersAdapter(
     private val imageFilters: List<ImageFilter>,
     private val imageFilterListener: ImageFilterListener
 ) : RecyclerView.Adapter<ImageFiltersAdapter.ImageFilterViewHolder>() {
+
+    private var selectedFilterPosition = 0
+    private var previouslySelectedPosition = 0
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageFilterViewHolder {
@@ -28,8 +34,29 @@ class ImageFiltersAdapter(
                 binding.imageFilterPreview.setImageBitmap(filterPreview)
                 binding.textFilterName.text = name
                 binding.root.setOnClickListener {
-                    imageFilterListener.onFilterSelected(this)
+                    if (position != selectedFilterPosition) {
+                        imageFilterListener.onFilterSelected(this)
+                        previouslySelectedPosition = selectedFilterPosition
+                        selectedFilterPosition = position
+                        with(this@ImageFiltersAdapter) {
+                            notifyItemChanged(previouslySelectedPosition, Unit)
+                            notifyItemChanged(selectedFilterPosition, Unit)
+                        }
+                    }
                 }
+            }
+            binding.textFilterName.apply {
+                setTextColor(
+                    ContextCompat.getColor(
+                        binding.textFilterName.context,
+                        if (selectedFilterPosition == position) {
+                            R.color.primaryDark
+                        } else {
+                            R.color.primaryText
+                        }
+                    )
+                )
+                textSize = if (selectedFilterPosition == position) 20f else 15f
             }
         }
     }
