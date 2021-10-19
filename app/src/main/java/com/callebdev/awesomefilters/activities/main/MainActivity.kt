7a9159fb0e8791755ffcc616af83ac3e499f1257
callebdev <callebmiquissene@gmail.com>
2021-10-19 21:25:06 +0200

    fix: remove deprecated onActivityResult

--- app/src/main/java/com/callebdev/awesomefilters/activities/main/MainActivity.kt
+++ app/src/main/java/com/callebdev/awesomefilters/activities/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.callebdev.awesomefilters.activities.main
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.callebdev.awesomefilters.activities.editimage.EditImageActivity
 import com.callebdev.awesomefilters.activities.savedimages.SavedImagesActivity
@@ -18,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { onImagePicked(it) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,12 +31,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupListeners() {
         binding.buttonEditImageFromGallery.setOnClickListener {
+
             Intent(
                 Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             ).also { pickerIntent ->
                 pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+                resultLauncher.launch(pickerIntent)
             }
         }
 
@@ -48,11 +53,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
-            data?.data?.let { imageUri ->
+    private fun onImagePicked(activityResult: ActivityResult) {
+        if (activityResult.resultCode == RESULT_OK) {
+            activityResult.data?.data?.let { imageUri ->
                 Intent(applicationContext, EditImageActivity::class.java).also { editImageIntent ->
                     editImageIntent.putExtra(KEY_IMAGE_URI, imageUri)
                     startActivity(editImageIntent)
